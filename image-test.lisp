a86d04bc7bd10029647217088e5fbf5154eff79e
Goran Pjević <67331732+goranpjevic@users.noreply.github.com>
2021-12-23 14:10:37 +0100

    fix image creation
    the rgb values of each pixel in the created image can now be of any
    value in the range [0, 255], instead of just in the range [128, 255].

--- image-test.lisp
+++ image-test.lisp
@@ -4,7 +4,6 @@
 
 ; create a 100×100px image file named 'test.png'
 ; the rgb values of all pixels are 200
-; note: all values can only be between 128 and 255, since they must be of type
-; '(unsigned-byte 8)
-(let ((img (april:april "100 100 3⍴200")))
+; 3⎕dt coerces the array to be of type '(unsigned-byte 8)
+(let ((img (april:april "3⎕dt 100 100 3⍴200")))
   (opticl:write-png-file "test.png" img))

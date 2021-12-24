86ab31e4daedfacca32cec7401a89b4663d4fe32
Goran Pjević <67331732+goranpjevic@users.noreply.github.com>
2021-12-24 10:58:15 +0100

    add new image test
    the example shows reading png files using the 'opticl' library, and then
    computing the data using the 'april' library.

--- image-test.lisp
+++ image-test.lisp
@@ -7,3 +7,9 @@
 ; 3⎕dt coerces the array to be of type '(unsigned-byte 8)
 (let ((img (april:april "3⎕dt 100 100 3⍴200")))
   (opticl:write-png-file "test.png" img))
+
+(let ((img (opticl:read-png-file "test.png")))
+  ; print all unique rgb values in the 'test.png' file
+  (print (april:april-c "{∪,⍵}" img))
+  ; print the average of all rgb valuse in the 'test.png' file
+  (print (april:april-c "{(+/÷≢),⍵}" img)))

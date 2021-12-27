b3db9b2bfe12f15d06a194e20d11377c324d1248
Goran Pjević <67331732+goranpjevic@users.noreply.github.com>
2021-12-27 07:00:24 +0100

    add test images to the repository
    the test images are now added to the repository.
    
    they are now also saved in the 'images/' directory.

--- image-test.lisp
+++ image-test.lisp
@@ -6,9 +6,9 @@
 ; the rgb values of all pixels are 200
 ; 3⎕dt coerces the array to be of type '(unsigned-byte 8)
 (let ((img (april:april "3⎕dt 100 100 3⍴200")))
-  (opticl:write-png-file "test.png" img))
+  (opticl:write-png-file "images/test.png" img))
 
-(let ((img (opticl:read-png-file "test.png")))
+(let ((img (opticl:read-png-file "images/test.png")))
   ; print all unique rgb values in the 'test.png' file
   (print (april:april-c "{∪,⍵}" img))
   ; print the average of all rgb valuse in the 'test.png' file
@@ -19,4 +19,4 @@
   (april:april-c "{[i;x;y;v] dim←⍴i⋄3⎕dt dim⍴(v@((⍳3⊃dim)+((3⊃dim)×¯1+x+(¯1+y)×2⊃dim))),i}" img x y vals))
 
 (let ((img (april:april "3⎕dt 30 30 3⍴200")))
-    (opticl:write-png-file "test2.png" (set-position img 10 10 #(100 100 100))))
+    (opticl:write-png-file "images/test2.png" (set-position img 10 10 #(100 100 100))))
